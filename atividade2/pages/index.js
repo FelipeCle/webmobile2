0abdd21fe2afc8cedd223ee889b802c647ca1f5f
Felipe Clé <94229656+FelipeCle@users.noreply.github.com>
2025-05-07 20:51:51 -0300

    Update index.js

--- atividade2/pages/index.js
+++ atividade2/pages/index.js
@@ -7,17 +7,21 @@ export default function HomePage({ products }) {
       <Link href="/novo-produto">
         <button style={{ marginBottom: '20px' }}>Adicionar Produto</button>
       </Link>
+
       {products.length === 0 ? (
         <p>Nenhum produto cadastrado.</p>
       ) : (
-        <ul className="product-list">
-          {products.map((product, index) => (
-            <li key={index} className="product-item">
-              <h3>{product.name}</h3>
-              <p>Preço: R$ {product.price}</p>
-            </li>
-          ))}
-        </ul>
+        <>
+          <h2 className="product-title">Lista de Produtos</h2>
+          <ul className="product-list">
+            {products.map((product, index) => (
+              <li key={index} className="product-item">
+                <h3>{product.name}</h3>
+                <p>Preço: R$ {product.price}</p>
+              </li>
+            ))}
+          </ul>
+        </>
       )}
     </div>
   );
